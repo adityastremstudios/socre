ee20c113689af75808cf665f2215eb165c4e4d23
adityastremstudios <aslovesh@gmail.com>
2025-09-17 02:07:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { TeamsProvider } from "./context/TeamsContext";
+import TeamsPage from "./pages/TeamsPage";
+import Scoreboard from "./pages/Scoreboard";
+
+function App() {
+  return (
+    <TeamsProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TeamsPage />} />
+          <Route path="/scoreboard" element={<Scoreboard />} />
+        </Routes>
+      </Router>
+    </TeamsProvider>
+  );
+}
+
+export default App;
 
