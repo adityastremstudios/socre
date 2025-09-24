0874399a35a28be16e09b063d5ebeaf1f5ad1b14
adityastremstudios <aslovesh@gmail.com>
2025-09-24 21:16:21 +0530

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,15 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- apiKey: "AIzaSyBH6Y61lrJWO6cFYklgfco3O5v7GfA4D8c",
+  apiKey: "AIzaSyBH6Y61lrJWO6cFYklgfco3O5v7GfA4D8c",
   authDomain: "for-alive.firebaseapp.com",
-  databaseURL: "https://for-alive-default-rtdb.firebaseio.com", // ✅ use this!
+  databaseURL: "https://for-alive-default-rtdb.firebaseio.com", // ✅ important
   projectId: "for-alive",
   storageBucket: "for-alive.appspot.com",
   messagingSenderId: "830764636345",
@@ -17,6 +12,5 @@ const firebaseConfig = {
   measurementId: "G-6244M0TTXW"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getDatabase(app);
