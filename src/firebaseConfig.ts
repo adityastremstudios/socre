c88a0a137e13321646b4cb11973bf7241ebefdf3
adityastremstudios <aslovesh@gmail.com>
2025-09-24 18:05:17 +0530

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,10 +7,11 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBH6Y61lrJWO6cFYklgfco3O5v7GfA4D8c",
+ apiKey: "AIzaSyBH6Y61lrJWO6cFYklgfco3O5v7GfA4D8c",
   authDomain: "for-alive.firebaseapp.com",
+  databaseURL: "https://for-alive-default-rtdb.firebaseio.com", // ✅ use this!
   projectId: "for-alive",
-  storageBucket: "for-alive.firebasestorage.app",
+  storageBucket: "for-alive.appspot.com",
   messagingSenderId: "830764636345",
   appId: "1:830764636345:web:edfa0d68b8481defa36368",
   measurementId: "G-6244M0TTXW"
