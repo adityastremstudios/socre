19a34543e7cfef7a640ed93f32ca95276fe6328b
INDxSnipzY <snipzy.offical@gmail.com>
2025-10-02 09:40:31 +0530

    Update TeamsPage.tsx

--- src/pages/TeamsPage.tsx
+++ src/pages/TeamsPage.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TeamsContext } from "../context/TeamsContext";
+import "./TeamPage.css";
 
 export default function TeamsPage() {
   const { teams, setTeams } = useContext(TeamsContext);
