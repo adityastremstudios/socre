59fe3b3fe6a26ff94164429a9843eda0937a6a7e
adityastremstudios <aslovesh@gmail.com>
2025-10-08 04:19:10 +0530

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,9 +4,9 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyBH6Y61lrJWO6cFYklgfco3O5v7GfA4D8c",
   authDomain: "for-alive.firebaseapp.com",
-  databaseURL: "https://for-alive-default-rtdb.firebaseio.com", // ✅ important
+  databaseURL: "https://for-alive-default-rtdb.firebaseio.com",
   projectId: "for-alive",
-  storageBucket: "for-alive.appspot.com",
+  storageBucket: "for-alive.firebasestorage.app",
   messagingSenderId: "830764636345",
   appId: "1:830764636345:web:edfa0d68b8481defa36368",
   measurementId: "G-6244M0TTXW"
